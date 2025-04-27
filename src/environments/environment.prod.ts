1ddfdfa795dcd828ae672864785b0e41b24029cf
jsalinas4 <jsalinasr3@upao.edu.pe>
2025-04-26 23:57:38 -0500

    v0.0.1
    Angular

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://your-production-api-base-url.com/api/v1'
+  };
+// The list of file replacements can be found in `angular.json`.
